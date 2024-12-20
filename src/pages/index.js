9df27ab77fcc3b98399fa09ef023f12fb9a249af
Rahul <rahul.nagar@techchefz.com>
2024-12-20 15:45:25 +0530

    add

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,22 +7,26 @@ const ProtectedPage = () => {
     <div>
       <h1>Welcome to the Protected Page</h1>
       <p>You have access to this page because you are authenticated.</p>
-      <Image 
-            src="https://media.istockphoto.com/id/177428869/photo/men-without-eyes.jpg?s=2048x2048&w=is&k=20&c=aI-w0YJ3fzcxBraO1LdNjnUIrXjo8emjmpoR_YQIFUo=" 
-            alt="alt" 
-            width={500} 
-            height={500} />
+      <Image
+        src="https://media.istockphoto.com/id/177428869/photo/men-without-eyes.jpg?s=2048x2048&w=is&k=20&c=aI-w0YJ3fzcxBraO1LdNjnUIrXjo8emjmpoR_YQIFUo="
+        alt="alt"
+        width={500}
+        height={500} />
     </div>
   );
 };
 
-const VerifyToken = withAuthToken(ProtectedPage);
+const Block1 = withAuthToken(ProtectedPage);
+const Block2 = withAuthToken(ProtectedPage);
 
 function home() {
   return (
     <div className={styles.page}>
-        <h1>Home</h1>
-        <VerifyToken />
+      <h1>Home</h1>
+      <div className={styles.block}>
+        <Block1 />
+        <Block2 />
+      </div>
     </div>
   )
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter} from "next/router"
 import styles from "../styles/login.module.css"
+import axios from 'axios';
 
 const LoginPage = () => {
+  const router = useRouter();
+  const [userName, setUserName] = useState("q222wereedqwer");
+  const [password, setPassword] = useState("2asdsddasd");
+
   return (
     <div className={styles.page}>
       <div className={styles.main}>
@@ -15,8 +21,10 @@ const LoginPage = () => {
               Username
             </label>
             <input
+              onChange={(e)=>setUserName(e.target.value)}
               id="username"
               type="text"
+              value={userName}
               placeholder="Enter your username"
               className="block w-full px-4 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
             />
@@ -27,7 +35,9 @@ const LoginPage = () => {
               Password
             </label>
             <input
+              onChange={(e)=>setPassword(e.target.value)}
               id="password"
+              value={password}
               type="password"
               placeholder="Enter your password"
               className="block w-full px-4 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
@@ -35,6 +45,24 @@ const LoginPage = () => {
           </div>
           {/* Submit Button */}
           <button
+            onClick={async (e)=>{
+              e.preventDefault();
+              if(userName!=="" && password!==""){
+                try {
+                  const getLogin = await axios.post("http://localhost:4000/api/verifyPassword",
+                    {
+                      username: userName,
+                      password: password
+                    }
+                  )
+                  sessionStorage.setItem("accessToken", getLogin?.data?.accessToken)
+                  sessionStorage.setItem("refreshToken", getLogin?.data?.refreshToken)
+                  await router.push("/");
+                } catch (error) {
+                  console.log("Error 51", error)
+                }
+              }
+            }}
             type="submit"
             className="w-full px-4 py-2 text-white bg-blue-600 rounded-md shadow hover:bg-blue-700 focus:ring-2 focus:ring-blue-500"
           >
